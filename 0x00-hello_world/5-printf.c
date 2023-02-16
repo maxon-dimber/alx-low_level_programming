793d10c32d60d94422203ea194669b53e2bbe69a
Maxon Onyango <mxondimber@gmail.com>
2023-02-16 11:15:03 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * A program that prints using the printf function
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
