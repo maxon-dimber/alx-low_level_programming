f4eab11bc701e1901d85cb0d3c21dce3b99ffe98
Maxon Onyango <mxondimber@gmail.com>
2023-02-16 10:59:47 -0800

    3-name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * A program that prints out text without using the printf functio
+ */
+int main(void)
+{
+	puts ("Programming is like building a multilingual puzzle"/n);
+	return (0);
+}
