377acab4981a417f6573cc480d9cf7311d1036e9
Maxon Onyango <mxondimber@gmail.com>
2023-02-20 03:17:23 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * prints 1 to 10 in decimal
+ * Return: Always (0) (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
